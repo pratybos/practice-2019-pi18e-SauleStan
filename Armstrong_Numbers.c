09188327d0240893a66ce04b3c30317fd7c309e3
SauleStan <47312637+SauleStan@users.noreply.github.com>
2019-02-04 09:08:54 +0200

    Update Armstrong_Numbers.c

--- Armstrong_Numbers.c
+++ Armstrong_Numbers.c
@@ -1,7 +1,6 @@
  #include <stdio.h>
 
-int main()
-{
+int main(){	
 	int a, b, c;
 	int abc, a3b3c3;
 	int count;
@@ -11,32 +10,23 @@ int main()
 	b = 0;
 	c = 0;
   
-	while(1)
-	{
+	while(1){
 		abc = a * 100 + b * 10 + c;
 		a3b3c3 = a*a*a + b*b*b + c*c*c;
 
-		if (abc > 999)
-		{
+		if (abc > 999){
 			break;
 		}
-
-		if (abc == a3b3c3)
-		{
+		if (abc == a3b3c3){
 			count = count + 1;
 			printf("Armstrong number %d : %d\n", count, abc);
 		}
-    
 		c++;
-    
-		if (c == 9)
-		{
+		if (c == 9){
 			c = 0;
 			b++;
 		}
-
-		if (b == 9)
-		{
+		if (b == 9){
 			b = 0;
 			a++;
 		}
