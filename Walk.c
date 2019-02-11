8e4d41620d485299c715de1f183b9d140dcbab65
SauleStan <47312637+SauleStan@users.noreply.github.com>
2019-02-11 08:07:26 +0200

    walk_with_function.c

--- Walk.c
+++ Walk.c
@@ -3,11 +3,9 @@
 	2019-02-05
 */
 
-//make leash a function.
-//add inventory? stack twigs, snacks for dog.
-//add situation with other people/ other dogs/animals.
-
 #include <stdio.h>
+
+int leash();
 
 int main() {
 	int dog;
@@ -20,7 +18,7 @@ int main() {
 		system("PAUSE");
 		main();
 	}
-	
+
 	if (dog == 0 || dog < 0) {
 		printf("You are out of dogs\n");
 		printf("Would you like to try again? (1 for YES, 0 for NO)\n");
@@ -33,33 +31,38 @@ int main() {
 		}
 	}
 	else {
-		printf("Do you have a leash? (1 for YES, 0 for NO)\n");
-		scanf_s("%d", &n);
-		if (n == 0) {
-			printf("Your dog ran away ;-; \n");
-			printf("would you like to call your dog? (1 for YES, 0 for NO)\n");
-			scanf_s("%d", &c);
-			if (c == 0) {
-				printf("You are out of dogs\n");
-				printf("Would you like to try again? (1 for YES, 0 for NO)\n");
-				scanf_s("%d", &t);
-				if (t == 1) {
-					main();
-				}
-				else {
-					return 0;
-				}
-
-			}
-			else {
-				printf("You got your dog back\n");
-			}
-		}
-		else
-			printf("You succesfully walked your dog\n");
+		leash();
 	}
 
-
-	system("PAUSE");
 	return 0;
+}
+
+int leash() {
+	int n, c, t;
+	printf("Do you have a leash? (1 for YES, 0 for NO)\n");
+	scanf_s("%d", &n);
+	if (n == 0) {
+		printf("Your dog ran away ;-; \n");
+		printf("would you like to call your dog? (1 for YES, 0 for NO)\n");
+		scanf_s("%d", &c);
+		if (c == 0) {
+			printf("You are out of dogs\n");
+			printf("Would you like to try again? (1 for YES, 0 for NO)\n");
+			scanf_s("%d", &t);
+			if (t == 1) {
+				main();
+			}
+			else {
+				return 0;
+			}
+
+		}
+		else {
+			printf("You got your dog back\n");
+		}
+	}
+	else
+		printf("You succesfully walked your dog\n");
+		system("PAUSE");
+
 }
